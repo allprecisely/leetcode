b9a26b379809aa0ce0bf4b746ec215eabe43ab3e
Ilnar Gilfanov <igilfanov@vivid.money>
2022-09-13 20:07:27 +0300

    4 after wrong answers

--- solutions/findMedianSortedArrays_4.py
+++ solutions/findMedianSortedArrays_4.py
@@ -9,7 +9,7 @@ class Solution:
         num_ammount = len_nums1 + len_nums2
         is_odd = num_ammount % 2
         half = num_ammount // 2 - (not is_odd)
-        half_answer = 0
+        half_answer = None
         while True:
             if i1 == len_nums1:
                 i, nums = i2, nums2
@@ -22,7 +22,7 @@ class Solution:
                 if i1 + i2 >= half:
                     if is_odd:
                         return nums1[i1]
-                    if half_answer:
+                    if half_answer is not None:
                         return (half_answer + nums1[i1]) / 2
                     half_answer = nums1[i1]
                 i1 += 1
@@ -30,7 +30,7 @@ class Solution:
                 if i1 + i2 >= half:
                     if is_odd:
                         return nums2[i2]
-                    if half_answer:
+                    if half_answer is not None:
                         return (half_answer + nums2[i2]) / 2
                     half_answer = nums2[i2]
                 i2 += 1
@@ -38,8 +38,8 @@ class Solution:
         if i1 + i2 >= half:
             if is_odd:
                 return nums[i]
-            if half_answer:
+            if half_answer is not None:
                 return (half_answer + nums[i]) / 2
-        half -= (num_ammount - i)
+        half = abs(len_nums2 - len_nums1) // 2 - (not is_odd)
         return nums[half] if is_odd else (nums[half] + nums[half + 1]) / 2
 
--- tests/test_4.py
+++ tests/test_4.py
@@ -23,7 +23,10 @@ instance = Solution()
         (([1, 2, 3, 11, 19, 20], [10, 18]), 10.5),
         (([-111, 111], [-20, -10]), -15),
         (([-1], [0]), -0.5),
+        # wrong answers
+        (([0, 0], [0, 0]), 0),
+        (([2], [1, 3, 4, 5, 6]), 3.5),
     ],
 )
 def test_solution(input, output):
-    assert instance.findMedianSortedArrays(*input) == output
+    assert output == instance.findMedianSortedArrays(*input)
