e9d6351020c1f5925fe214ac56a1169b14b5135f
Ilnar Gilfanov <igilfanov@vivid.money>
2022-09-13 19:43:02 +0300

    4 findMedianSortedArrays

---
+++ solutions/findMedianSortedArrays_4.py
@@ -0,0 +1,45 @@
+# 4. Median of Two Sorted Arrays
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        i1 = i2 = 0
+        len_nums1, len_nums2 = len(nums1), len(nums2)
+        num_ammount = len_nums1 + len_nums2
+        is_odd = num_ammount % 2
+        half = num_ammount // 2 - (not is_odd)
+        half_answer = 0
+        while True:
+            if i1 == len_nums1:
+                i, nums = i2, nums2
+                break
+            elif i2 == len_nums2:
+                i, nums = i1, nums1
+                break
+
+            if nums1[i1] <= nums2[i2]:
+                if i1 + i2 >= half:
+                    if is_odd:
+                        return nums1[i1]
+                    if half_answer:
+                        return (half_answer + nums1[i1]) / 2
+                    half_answer = nums1[i1]
+                i1 += 1
+            else:
+                if i1 + i2 >= half:
+                    if is_odd:
+                        return nums2[i2]
+                    if half_answer:
+                        return (half_answer + nums2[i2]) / 2
+                    half_answer = nums2[i2]
+                i2 += 1
+
+        if i1 + i2 >= half:
+            if is_odd:
+                return nums[i]
+            if half_answer:
+                return (half_answer + nums[i]) / 2
+        half -= (num_ammount - i)
+        return nums[half] if is_odd else (nums[half] + nums[half + 1]) / 2
+
---
+++ tests/test_4.py
@@ -0,0 +1,29 @@
+import pytest
+
+from solutions.findMedianSortedArrays_4 import Solution
+
+instance = Solution()
+
+
+@pytest.mark.parametrize(
+    'input, output',
+    [
+        (([1, 3], [2]), 2),
+        (([1, 2], [3, 4]), 2.5),
+        # my checks
+        (([1], []), 1),
+        (([1, 1], []), 1),
+        (([1, 2], []), 1.5),
+        (([1, 2, 3], []), 2),
+        (([1, 2, 3, 100], []), 2.5),
+        (([], [1]), 1),
+        (([1], [1]), 1),
+        (([1], [2]), 1.5),
+        (([1, 2, 3, 18, 19, 20], [10, 11]), 10.5),
+        (([1, 2, 3, 11, 19, 20], [10, 18]), 10.5),
+        (([-111, 111], [-20, -10]), -15),
+        (([-1], [0]), -0.5),
+    ],
+)
+def test_solution(input, output):
+    assert instance.findMedianSortedArrays(*input) == output
