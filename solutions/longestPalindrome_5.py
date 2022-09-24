195a92f6a81361c3e221a75fed3483d7b95ad868
Ilnar Gilfanov <allprecisely@gmail.com>
2022-09-24 23:01:15 +0300

    5 longestPalindrome fixed

--- solutions/longestPalindrome_5.py
+++ solutions/longestPalindrome_5.py
@@ -3,31 +3,13 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        current_palindrome_start_index = 0
-        current_palindrome_length = 1
-        longest_palindrome_start_index = 0
-        longest_palindrome_length = 1
-        for i, c in enumerate(s[1:], 1):
-            if current_palindrome_start_index - 1 < 0:
-                current_palindrome_start_index = i - 1
-                current_palindrome_length = 1
-            if (
-                current_palindrome_start_index - 1 >= 0
-                and s[current_palindrome_start_index - 1] == c
-            ):
-                current_palindrome_start_index -= 1
-                current_palindrome_length += 2
-            elif s[i - 1] == c:
-                current_palindrome_start_index = i - 1
-                current_palindrome_length = 2
-            else:
-                current_palindrome_start_index = i
-                current_palindrome_length = 1
+        res = ''
+        for i in range(len(s)):
+            res = max(res, self.helper(s, i, i), self.helper(s, i, i + 1), key=len)
+        return res
+    
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l, r = l - 1, r + 1
+        return s[l + 1 : r]
 
-            if longest_palindrome_length < current_palindrome_length:
-                longest_palindrome_length = current_palindrome_length
-                longest_palindrome_start_index = current_palindrome_start_index
-        return s[
-            longest_palindrome_start_index : longest_palindrome_start_index
-            + longest_palindrome_length
-        ]
