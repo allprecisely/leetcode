aab22d90aa3370de5f37c7825ffae40fc4e671dd
Ilnar Gilfanov <allprecisely@gmail.com>
2023-12-09 17:00:38 +0200

    problem_1_two_sum improved

--- solutions/problem_1_two_sum.py
+++ solutions/problem_1_two_sum.py
@@ -3,17 +3,8 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        set_nums = set(nums)
-        first_index = None
-        for num in set_nums:
-            if target - num in set_nums:
-                if num == target - num:
-                    for i, _num in enumerate(nums):
-                        if num == _num:
-                            if first_index == None:
-                                first_index = i
-                            else:
-                                return [first_index, i]
-                    first_index = None
-                else:
-                    return [nums.index(num), nums.index(target - num)]
+        nums_indexes = {}
+        for i, num in enumerate(nums):
+            if target - num in nums_indexes:
+                return [i, nums_indexes[target - num]]
+            nums_indexes[num] = i
