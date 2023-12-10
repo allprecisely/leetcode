957d39630bedbdc3b5fe7f6505e9ad2fa10ab0f3
Ilnar Gilfanov <allprecisely@gmail.com>
2023-12-10 11:47:46 +0200

    problem_238_product_of_array_except_self improved

--- solutions/problem_238_product_of_array_except_self.py
+++ solutions/problem_238_product_of_array_except_self.py
@@ -1,12 +1,17 @@
 # https://leetcode.com/problems/product-of-array-except-self/description/
+# 1 try; but not memory optimal, so improved
 from typing import List
 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         len_nums = len(nums)
-        left_products, right_products = [1] * len_nums, [1] * len_nums
-        for i in range(len_nums - 1):
-            left_products[i + 1] = left_products[i] * nums[i]
-            right_products[i + 1] = right_products[i] * nums[-i - 1]
-        return [left_products[i] * right_products[-i - 1] for i in range(len_nums)]
+        answer = [1] * len_nums
+        left_product = right_product = 1
+        for i in range(len_nums):
+            answer[i] *= left_product
+            left_product *= nums[i]
+            answer[-i - 1] *= right_product
+            right_product *= nums[-i - 1]
+        return answer
+        
