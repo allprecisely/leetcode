8acecac6ceaab06681ca1ebce36be8b7fe2a378c
Ilnar Gilfanov <allprecisely@gmail.com>
2023-12-11 19:03:18 +0200

    problem_443_string_compression improved

--- solutions/problem_443_string_compression.py
+++ solutions/problem_443_string_compression.py
@@ -1,31 +1,27 @@
 # https://leetcode.com/problems/string-compression/description/
+# 1 try; improved a little, but it's mostly the same
 
 from typing import List
 
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        result = 0
-        group_char = chars[0]
-        group_length = 1
-        for c in chars[1:]:
-            if c == group_char:
-                group_length += 1
-            else:
-                result += self.handle_group(chars, group_char, group_length, result)
-                group_char = c
-                group_length = 1
-        result += self.handle_group(chars, group_char, group_length, result)
-        return result
+        chars_len = len(chars)
 
-    def handle_group(
-        self, chars: List[str], group_char: str, group_length: int, last_index: int
-    ) -> int:
-        group_compressed_length = 1
-        chars[last_index] = group_char
-        if group_length > 1:
-            group_length_str = str(group_length)
-            for i, digit in enumerate(group_length_str, last_index + 1):
-                chars[i] = digit
-            group_compressed_length += len(group_length_str)
-        return group_compressed_length
+        if chars_len == 1:
+            return 1
+        
+        result_length = group_start = 0
+        
+        for i, c in enumerate(chars):
+            if chars_len == i + 1 or c != chars[i + 1]:
+                chars[result_length] = c
+                result_length += 1
+
+                if i > group_start:
+                    group_lenght = i - group_start + 1
+                    for digit in str(group_lenght):
+                        chars[result_length] = digit
+                        result_length += 1
+                group_start = i + 1
+        return result_length
