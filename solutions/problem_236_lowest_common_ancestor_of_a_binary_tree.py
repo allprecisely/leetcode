eb76ac1b110db76662a63b0b863af56e808cc7c6
Ilnar Gilfanov <allprecisely@gmail.com>
2023-12-17 17:12:49 +0200

    problem_236_lowest_common_ancestor_of_a_binary_tree improved

--- solutions/problem_236_lowest_common_ancestor_of_a_binary_tree.py
+++ solutions/problem_236_lowest_common_ancestor_of_a_binary_tree.py
@@ -1,9 +1,5 @@
 # https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 
-
-from typing import List
-
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -13,17 +9,8 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.ancestor = None
-        self.dfs(root, [p, q])
-        return self.ancestor
-
-    def dfs(self, root: 'TreeNode', not_found: List['TreeNode']) -> List['TreeNode']:
-        if root == None or self.ancestor != None:
-            return []
-
-        found = self.dfs(root.left, not_found) + self.dfs(root.right, not_found)
-        if root in not_found:
-            found.append(root)
-        if self.ancestor == None and set(found) == set(not_found):
-            self.ancestor = root
-        return found
+        if root in (None, p, q):
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        return root if left and right else left or right
